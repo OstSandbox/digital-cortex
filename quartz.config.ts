4c17019e9a5fd1f6fbda9a3c51a98ad8eedc8a65
OstSandbox <zartanin@bk.ru>
2025-07-06 17:52:55 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "digital-cortex",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
